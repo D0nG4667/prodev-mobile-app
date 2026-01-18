b9890d69ddf674202d6ddaecd06c3f804bd49bea
D0nG4667 <D0nG4667@users.noreply.github.com>
2026-01-19 02:23:21 +0300

    fix: requirement

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -29,6 +29,7 @@ const HomeRootLayout = () => {
       }} />
         <Tabs.Screen name="profile" options={{
         title: 'Profile',
+        headerShown: false,
         tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
       }} />
     </Tabs>
